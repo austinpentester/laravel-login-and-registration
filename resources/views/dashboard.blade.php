57c87faf8c9d829b32213e8114db91ea59c8bf14
austinpentester <austinpentester@gmail.com>
2022-10-24 10:42:33 +0530

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+
+@section('content')
+
+<div class="card">
+	<div class="card-header">Dashboard</div>
+	<div class="card-body">
+
+		You are Login in  Login Registration Application. <br>
+        Hi User
+	</div>
+</div>
+
+@endsection('content')
